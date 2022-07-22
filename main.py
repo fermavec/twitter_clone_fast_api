a48ada50b7b8bcff7852f989d46066db770d7ee3
Fernando Mavec <fermavec@gmail.com>
2022-07-22 12:30:28 -0500

    pathoperation/tweet

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 # Path Operations
@@ -76,9 +76,7 @@ class Tweet(BaseModel):
 def signup(user: UserRegister = Body(...)): 
     """
     Signup
-
     This path operation register a user in the app
-
     Parameters: 
         - Request body parameter
             - user: UserRegister
@@ -123,10 +121,8 @@ def login():
 def show_all_users(): 
     """
     This path operation shows all users in the app
-
     Parameters: 
         -
-
     Returns a json list with all users in the app, with the following keys: 
         - user_id: UUID
         - email: Emailstr
@@ -193,8 +189,34 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post(): 
-    pass
+def post(tweet: Tweet = Body(...)): 
+    """
+    Post a Tweet
+    This path operation post a tweet in the app
+    Parameters: 
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a json with the basic tweet information: 
+        tweet_id: UUID  
+        content: str    
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f: 
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
